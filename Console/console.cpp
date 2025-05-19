a555476a2767e442d7b897ff416dedea2f0f04fd
haung1057 <15036632+huang-him@user.noreply.gitee.com>
2025-05-19 23:12:03 +0800

    修改main

--- Console/console.cpp
+++ Console/console.cpp
@@ -4,17 +4,5 @@
 
 int  main()
 {
-	InitEngine(640, 480, "BridgeEngine");
-	return 0;
-    switch (PLATFORM)
-    {
-    case PLATFORM_WINDOWS:
-        break;
-    case PLATFORM_LINUX:
-        break;
-    case PLATFORM_XJ380:
-        break;
-    default:
-        break;
-    }
+    InitEngine(640, 480, "BridgeEngine");
 }
