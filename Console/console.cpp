24bb67f1b67c331c7476d1bf7e156d8b1c8bd617
haung1057 <15036632+huang-him@user.noreply.gitee.com>
2025-05-19 23:05:28 +0800

    添加一个main函数（

--- Console/console.cpp
+++ Console/console.cpp
@@ -2,3 +2,19 @@
 #include <compile_set.h>
 
 
+int  main()
+{
+	InitEngine(640, 480, "BridgeEngine");
+	return 0;
+    switch (PLATFORM)
+    {
+    case PLATFORM_WINDOWS:
+        break;
+    case PLATFORM_LINUX:
+        break;
+    case PLATFORM_XJ380:
+        break;
+    default:
+        break;
+    }
+}
